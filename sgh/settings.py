60ef5530e18f6b817366a6a9c49c1cbe515f1af3
simondev413 <devemail413@gmail.com>
2026-01-27 06:41:32 +0100

    Adicionada configuração de servidor de  staticfiles

--- sgh/settings.py
+++ sgh/settings.py
@@ -156,6 +156,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- sgh/urls.py
+++ sgh/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_title = ' '
 admin.site.site_header = 'Administração do Sistema de Gestão Hospitalar'
@@ -102,3 +103,6 @@ urlpatterns = [
 
     path('api/v1/', include(routes.urls)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
